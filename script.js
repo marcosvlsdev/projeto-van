f801c791936988bbb43b23b4967f2bcb4cf3b06b
Marcos Vinicius <marcosviniciusleite08@gmail.com>
2024-10-28 21:06:18 -0300

    site pegando forma

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const imagens = document.querySelectorAll('.carrosel-conteiner img');
+const textos = document.querySelectorAll('.texto-sobre-imagem');
+let imagemAtiva = 0; 
+
+function atualizarImagem() {
+  imagens.forEach((img, index) => {
+    if (index === imagemAtiva) {
+      img.classList.add('active'); 
+      textos[index].style.display = 'block'; 
+    } else {
+      img.classList.remove('active');
+      textos[index].style.display = 'none'; 
+    }
+  });
+}
+
+function prevImg() {
+  imagemAtiva = (imagemAtiva === 0) ? imagens.length - 1 : imagemAtiva - 1;
+  atualizarImagem();
+}
+
+
+function nextImg() {
+  imagemAtiva = (imagemAtiva === imagens.length - 1) ? 0 : imagemAtiva + 1;
+  atualizarImagem();
+}
+
+
+atualizarImagem();
